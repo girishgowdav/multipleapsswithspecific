a58459ddad7dc4a410eaefe0723662ca7d75118b
girishgowdav <gowdagirishv38@gmail.com>
2023-05-26 12:58:07 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from app1.views import *
+app_name='anything'
+urlpatterns=[
+
+path('giri1/',giri1,name='giri1'),
+]
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def giri2(request):
+    return HttpResponse('hello google')
